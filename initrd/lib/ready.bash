122662a9abf630ca042466196464a851991d6eaf
Black-Hole <bh@bugs.cc>
2023-10-18 11:06:13 +0800

    refactor(initrd): improve initrd build (#13)

--- initrd/lib/ready.bash
+++ initrd/lib/ready.bash
@@ -5,19 +5,8 @@ set -e
 create_sysroot_dir() {
     section "Create directory structure in $ROOTFS"
 
-    mkdir -pv "$ROOTFS"
-
-    for i in bin cdrom dev etc proc sys mnt opt root run sbin tmp usr; do
-        mkdir -p "${ROOTFS}"/$i
-    done
-
-    mkdir -p "$ROOTFS"/usr/bin
-    mkdir -p "$ROOTFS"/usr/sbin
-    mkdir -p "$ROOTFS"/usr/etc
-    mkdir -p "$ROOTFS"/usr/lib
-    mkdir -p "$ROOTFS"/usr/libexec
-    mkdir -p "$ROOTFS"/usr/include
-    mkdir -p "$ROOTFS"/usr/share
+    mkdir -p "${ROOTFS}"/{bin,cdrom,dev,etc,proc,sys,mnt,opt,root,run,sbin,tmp,usr}
+    mkdir -p "$ROOTFS"/usr/{bin,sbin,etc,lib,libexec,include,share}
 
     ln -n -sf lib "$ROOTFS"/usr/lib64
     ln -n -sf usr/lib "$ROOTFS"/lib
