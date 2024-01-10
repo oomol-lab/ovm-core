85d85756a5a981c8bdd4721225c478200c14f9b6
Black-Hole1 <bh@bugs.cc>
2024-01-10 15:53:32 +0800

    refactor(buildroot): upgrade vsock-guest-exec version to v0.1.0
    Ref: https://github.com/oomol-lab/vsock-guest-exec/releases/tag/v0.1.0
    
    Signed-off-by: Black-Hole1 <bh@bugs.cc>

--- buildroot_external/package/vsock-guest-exec/vsock-guest-exec.mk
+++ buildroot_external/package/vsock-guest-exec/vsock-guest-exec.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VSOCK_GUEST_EXEC_VERSION = 0.0.1
+VSOCK_GUEST_EXEC_VERSION = 0.1.0
 VSOCK_GUEST_EXEC_SITE = $(call github,oomol-lab,vsock-guest-exec,v$(VSOCK_GUEST_EXEC_VERSION))
 
 VSOCK_GUEST_EXEC_LICENSE = MIT
