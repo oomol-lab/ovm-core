9bdf43145310cf4c64ac3d8164328ac3269bc4ae
Kevin Cui <bh@bugs.cc>
2024-01-30 17:16:38 +0800

    chore(buildroot): upgrade gvforwarder to 0.7.2
    Signed-off-by: Kevin Cui <bh@bugs.cc>

--- buildroot_external/package/gvforwarder/gvforwarder.mk
+++ buildroot_external/package/gvforwarder/gvforwarder.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GVFORWARDER_VERSION = 0.6.2
+GVFORWARDER_VERSION = 0.7.2
 GVFORWARDER_SITE = $(call github,containers,gvisor-tap-vsock,v$(GVFORWARDER_VERSION))
 
 GVFORWARDER_LICENSE = Apache-2.0
@@ -12,9 +12,6 @@ GVFORWARDER_LICENSE_FILES = LICENSE
 
 GVFORWARDER_BUILD_TARGETS = cmd/vm
 GVFORWARDER_LDFLAGS = -s -w
-
-GVFORWARDER_PATCH = \
-	https://patch-diff.githubusercontent.com/raw/containers/gvisor-tap-vsock/pull/215.patch
 
 define GVFORWARDER_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 $(@D)/bin/vm $(TARGET_DIR)/usr/bin/gvforwarder
